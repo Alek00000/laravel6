e3017e9e76f7b18345f6a5296dd6fb4f2c9de30f
Alek00000 <zekovic.aleksandra253@gmail.com>
2020-06-24 14:22:59 +0200

    Zavrsena 19. lekcija Views

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Article;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/articles/{article}', 'ArticleController@show');
+
+Route::get('/careers', function () {
+    return view('careers');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/clients', function () {
+    return view('clients');
+});
+
+
